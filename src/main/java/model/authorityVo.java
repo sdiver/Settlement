2e9714024eac33abc8bab2940986932635519576
sdiver <sdiver2012@126.com>
2017-03-01 10:32:10 +0800

    用户
    (cherry picked from commit c8d7d52220a07888d24db0dd584762c170ae68b9)
    
    # Conflicts:
    #	.idea/workspace.xml

---
+++ src/main/java/model/authorityVo.java
@@ -0,0 +1,37 @@
+package model;
+/**   
+* @Title: authorityVo
+* @Package model 
+* @Description: authorityVo.java
+* @author Sdiver 18605916639_wo_cn   
+* @date 2017/3/1 上午2:37 
+* @version V1.0   
+*/
+public class authorityVo {
+    private int authorityId;
+    private int ifAthority;
+
+    public int getAuthorityId() {
+        return authorityId;
+    }
+
+    public void setAuthorityId(int authorityId) {
+        this.authorityId = authorityId;
+    }
+
+    public int getIfAthority() {
+        return ifAthority;
+    }
+
+    public void setIfAthority(int ifAthority) {
+        this.ifAthority = ifAthority;
+    }
+
+    @Override
+    public String toString() {
+        return "authorityVo{" +
+                "authorityId=" + authorityId +
+                ", ifAthority=" + ifAthority +
+                '}';
+    }
+}
